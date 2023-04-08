359c752f3f7af462a4a035c9c67bd29bf44e7730
Callum Wellwood-Kane <business@callumwk.com>
2023-04-08 15:44:55 +1000

    wip: added row and form

--- src/pages/tracker.tsx
+++ src/pages/tracker.tsx
@@ -1,26 +1,9 @@
+import { AssetRow } from "@/components/asset-row";
 import { api } from "@/utils/api";
 import { Box, FormControl, Input, Text } from "@chakra-ui/react";
 import { type NextPage } from "next";
 
 const Tracker: NextPage = () => {
-  const data = api.assets.getAll.useQuery({ asset_id: 1 });
-  console.debug(data.data);
-
-  return (
-    <Box>
-      {data.data?.map((asset) => {
-        return (
-          <Box display="grid" gridTemplateColumns="1fr 1fr">
-            <FormControl>
-              <Input borderRadius={0} type="string" placeholder={asset.name} />
-            </FormControl>
-            <FormControl>
-              <Input value={asset.values[0]?.value} />
-            </FormControl>
-          </Box>
-        );
-      })}
-    </Box>
-  );
+  return <AssetRow />;
 };
 export default Tracker;
