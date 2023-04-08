f321511516a5eb32e84bfa6705bc5b9b40033266
Callum Wellwood-Kane <business@callumwk.com>
2023-04-08 16:08:41 +1000

    switched to npm, add clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
     </ChakraProvider>
   );
 };
