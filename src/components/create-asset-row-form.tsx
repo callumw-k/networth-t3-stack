43b3878f1768d8a989b8e8fc1858411c627943d0
Callum Wellwood-Kane <business@callumwk.com>
2023-04-25 14:28:19 +1000

    finalised button

--- src/components/create-asset-row-form.tsx
+++ src/components/create-asset-row-form.tsx
@@ -1,16 +1,21 @@
 import { api } from "@/utils/api";
-import { Box, FormControl, Input, Spinner } from "@chakra-ui/react";
+import { Box, Button, FormControl, Input, Spinner } from "@chakra-ui/react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { inputStyles, rowStyles } from "./asset-row-form";
 import { CheckIcon } from "@chakra-ui/icons";
 import cloneDeep from "lodash.clonedeep";
+import { useEffect, useState } from "react";
 
 type FormValues = {
   assetName: string;
   value: number;
 };
 
-export function CreateAssetRowForm() {
+type FormProps = {
+  setClickedAddAsset: (value: boolean) => void;
+};
+
+function Form(props: FormProps) {
   const {
     register,
     formState: { errors },
@@ -27,16 +32,20 @@ export function CreateAssetRowForm() {
       if (!assetsData || !data) return;
       assetsData.push(data);
       trpcContext.assets.getAllAssetsForUser.setData(undefined, assetsData);
+      props.setClickedAddAsset(false);
     },
   });
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     if (errors.assetName || errors.value) return;
     mutation.mutate({ assetName: data.assetName, assetValue: data.value });
-    reset();
   };
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, []);
   const correctValues = watch("assetName") && !isNaN(watch("value"));
   return (
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <form onSubmit={handleSubmit(onSubmit)}>
       <Box {...rowStyles}>
         <FormControl isInvalid={Boolean(errors.assetName)}>
@@ -66,5 +75,18 @@ export function CreateAssetRowForm() {
         )}
       </Box>
     </form>
+  );
+}
+
+export function CreateAssetRowForm() {
+  const [clickedAddAsset, setClickedAddAsset] = useState(false);
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <Box display={"flex"} alignItems={"flex-start"} flexDirection={"column"}>
+      {clickedAddAsset && <Form setClickedAddAsset={setClickedAddAsset} />}
+      <Button marginTop={4} onClick={() => setClickedAddAsset(true)}>
+        Add new asset
+      </Button>
+    </Box>
   );
 }
